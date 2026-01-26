94754c2b24ed5ae0619b65b4e424482e878c2f76
nhz <nichohz@gmail.com>
2026-01-25 21:21:39 -0600

    latest

--- Scenes/Main/level_manager.gd
+++ Scenes/Main/level_manager.gd
@@ -1,3 +1,18 @@
+# loads in levels and manages level transitions
 extends Node2D
 
-# loads in levels and manages level transitions
+
+@export var initial_level_scene : PackedScene
+func _ready() -> void:
+	if initial_level_scene:
+		load_scene(initial_level_scene)
+
+
+func load_scene(new_scene : PackedScene) -> void:
+	# remove all existing children (current level)
+	for child in get_children():
+		child.queue_free()
+	
+	# instantiate and add the new level scene
+	var level_instance = new_scene.instantiate()
+	add_child(level_instance)
