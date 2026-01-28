b443ed34f89f819cc5422b00e2248a8a6d115e76
Kevin Rose <kevin+gh@maypark.com>
2026-01-28 16:09:35 -0600

    Rework of card assets, just write them in code

--- Scenes/Battler/Card/all_possible_cards.gd
+++ Scenes/Battler/Card/all_possible_cards.gd
@@ -2,6 +2,25 @@ extends Node
 
 class_name AllPossibleCards
 
-const sip_drink = preload("res://Assets/CardResources/sip_drink.tres")
-const backflip = preload("res://Assets/CardResources/backflip.tres")
-const growl = preload("res://Assets/CardResources/growl.tres")
+
+static var sip_drink = CardResource.new(
+	"Sip Drink",
+	"Take sips of a drink with your weird hands.\n\n+2 Vibes\n-1 Sus",
+	[BattleScores.Effects.Change, BattleScores.Effects.Change],
+	[BattleScores.ScoreCategories.Vibes, BattleScores.ScoreCategories.Suspicion],
+	[2, -1]
+)
+static var backflip = CardResource.new(
+	"Back Flip",
+	"Could a raccoon do [i]this[/i]??\n\n[b][i]backflip[/i][/b]\n\n+1 Vibes\nB1 Sus",
+	[BattleScores.Effects.Change, BattleScores.Effects.Defend],
+	[BattleScores.ScoreCategories.Vibes, BattleScores.ScoreCategories.Suspicion],
+	[1, 1]
+)
+static var growl = CardResource.new(
+	"Growl",
+	"[i]grrrrrrrr[/i]\n\n+2 Fear\n-1 Sus",
+	[BattleScores.Effects.Change, BattleScores.Effects.Change],
+	[BattleScores.ScoreCategories.Fear, BattleScores.ScoreCategories.Suspicion],
+	[2, -1]
+)
--- Scenes/Battler/battle_scores.gd
+++ Scenes/Battler/battle_scores.gd
@@ -9,7 +9,6 @@ enum ScoreCategories {
 }
 
 enum Effects {
-	Increase,
+	Change,
 	Defend,
-	Decrease,
 }
